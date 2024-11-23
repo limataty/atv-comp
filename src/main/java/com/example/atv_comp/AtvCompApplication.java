a24053106fa8f5153b837b1db10f80a29110df26
tatey <tatyana.lima@sptech.school>
2024-11-23 17:27:51 -0300

    primeiro commit

---
+++ src/main/java/com/example/atv_comp/AtvCompApplication.java
@@ -0,0 +1,13 @@
+package com.example.atv_comp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvCompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtvCompApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/atv_comp/controller/CalculatorController.java
@@ -0,0 +1,32 @@
+package com.example.atv_comp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+    @GetMapping("/soma")
+    public double soma(@RequestParam double a, @RequestParam double b) {
+        return a + b;
+    }
+
+    @GetMapping("/subtracao")
+    public double subtracao(@RequestParam double a, @RequestParam double b) {
+        return a - b;
+    }
+
+    @GetMapping("/multiplicacao")
+    public double multiplicacao(@RequestParam double a, @RequestParam double b) {
+        return a * b;
+    }
+
+    @GetMapping("/divisao")
+    public double divisao(@RequestParam double a, @RequestParam double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Divisão por zero não é permitida.");
+        }
+        return a / b;
+    }
+}
